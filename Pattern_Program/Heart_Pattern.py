f85e133f338adc881a559fb03fcc73182f13e409
Labdhi Gathani <123727193+Labdhi47@users.noreply.github.com>
2023-06-15 12:18:43 +0530

    Update Heart_Pattern.py

--- Pattern_Program/Heart_Pattern.py
+++ Pattern_Program/Heart_Pattern.py
@@ -1,3 +1,10 @@
+#   ** ** 
+#  *  *  *
+#  *     *
+#   *   * 
+#    * *  
+#     *  
+
 for row in range(7):
     for col in range(7):
         if (col==0 and (row==1 or row==2)) or (col==1 and (row==0 or row==3)) or (col==2 and (row==0 or row==4)) or (col==3 and (row==1 or row==5)) or (col==4 and (row==0 or row==4)) or (col==5 and (row==0 or row==3)) or (col==6 and (row==1 or row==2)):
